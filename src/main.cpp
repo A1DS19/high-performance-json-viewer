675855139670cbd0bc4f5098ea7bacec3c605c13
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2026-02-02 13:44:41 -0600

    initial window display

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,97 @@
-#include "main.hpp"
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
 
-#include <cstdlib>
-#include <iostream>
-
-void hello() { std::cout << "Hello, World!" << "\n"; }
+#include <SDL3/SDL.h>
+#include <cstdio>
 
 int main() {
-  hello();
+  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
+    printf("Error: SDL_Init(): %s\n", SDL_GetError());
+    return 1;
+  }
 
-  return EXIT_SUCCESS;
+  float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+  SDL_WindowFlags window_flags =
+      SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+  SDL_Window *window = SDL_CreateWindow("JSON Viewer", (int)(1280 * main_scale),
+                                        (int)(800 * main_scale), window_flags);
+  if (window == nullptr) {
+    printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+  SDL_SetRenderVSync(renderer, 1);
+  if (renderer == nullptr) {
+    printf("Error: SDL_CreateRenderer(): %s\n", SDL_GetError());
+    return 1;
+  }
+  SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+  SDL_ShowWindow(window);
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &input_output = ImGui::GetIO();
+  input_output.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  input_output.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+  ImGui::StyleColorsDark();
+
+  ImGuiStyle &style = ImGui::GetStyle();
+  style.ScaleAllSizes(main_scale);
+  style.FontScaleDpi = main_scale;
+
+  ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+  ImGui_ImplSDLRenderer3_Init(renderer);
+
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  bool done = false;
+  while (!done) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      ImGui_ImplSDL3_ProcessEvent(&event);
+      if (event.type == SDL_EVENT_QUIT)
+        done = true;
+      if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED &&
+          event.window.windowID == SDL_GetWindowID(window))
+        done = true;
+    }
+
+    if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
+      SDL_Delay(10);
+      continue;
+    }
+
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+
+    // Your UI here
+    ImGui::Begin("JSON Viewer");
+    ImGui::Text("Hello!");
+    ImGui::ColorEdit3("clear color", (float *)&clear_color);
+    ImGui::Text("%.1f FPS", input_output.Framerate);
+    ImGui::End();
+
+    ImGui::Render();
+    SDL_SetRenderScale(renderer, input_output.DisplayFramebufferScale.x,
+                       input_output.DisplayFramebufferScale.y);
+    SDL_SetRenderDrawColorFloat(renderer, clear_color.x, clear_color.y,
+                                clear_color.z, clear_color.w);
+    SDL_RenderClear(renderer);
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  ImGui_ImplSDLRenderer3_Shutdown();
+  ImGui_ImplSDL3_Shutdown();
+  ImGui::DestroyContext();
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
